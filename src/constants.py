d3e8a9b3ee9ca1f0b58d4761a54655bdadf01573
Cole Khamnei <cole.khamnei@gmail.com>
2024-12-04 21:40:16 -0500

    added EDA

---
+++ src/constants.py
@@ -0,0 +1,15 @@
+import os
+
+# ----------------------------------------------------------------------------# 
+# -------------             Directory And File Paths             -------------# 
+# ----------------------------------------------------------------------------# 
+
+CONSTANTS_FILE_PATH = os.path.realpath(__file__)
+SRC_DIR_PATH = os.path.dirname(CONSTANTS_FILE_PATH)
+PROJECT_DIR_PATH = os.path.dirname(SRC_DIR_PATH)
+DATA_DIR_PATH = os.path.join(PROJECT_DIR_PATH, "data")
+THRYOID_CANCER_DATA_PATH = os.path.join(PROJECT_DIR_PATH, "thyroid_recurrence.csv")
+
+# ----------------------------------------------------------------------------# 
+# --------------------                End                 --------------------# 
+# ----------------------------------------------------------------------------#
