e96637a7e00386f9268fe9f63c6b4a6239bc31cd
Cole Khamnei <cole.khamnei@gmail.com>
2024-12-04 22:03:02 -0500

    rte

--- src/constants.py
+++ src/constants.py
@@ -8,7 +8,8 @@ CONSTANTS_FILE_PATH = os.path.realpath(__file__)
 SRC_DIR_PATH = os.path.dirname(CONSTANTS_FILE_PATH)
 PROJECT_DIR_PATH = os.path.dirname(SRC_DIR_PATH)
 DATA_DIR_PATH = os.path.join(PROJECT_DIR_PATH, "data")
-THRYOID_CANCER_DATA_PATH = os.path.join(PROJECT_DIR_PATH, "thyroid_recurrence.csv")
+
+THRYOID_CANCER_DATA_PATH = os.path.join(DATA_DIR_PATH, "thyroid_recurrence.csv")
 
 # ----------------------------------------------------------------------------# 
 # --------------------                End                 --------------------# 
